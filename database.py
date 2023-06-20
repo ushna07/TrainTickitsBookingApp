fdd54799cf298f4d5e39c27225a8eb2de03c191e
ushna roy <ushna02022001@gmail.com>
2023-06-20 10:46:01 +0530

    Code Updated
    Signed-off-by: ushna roy <ushna02022001@gmail.com>

---
+++ database.py
@@ -0,0 +1,16 @@
+from Station import Station
+
+trains = []
+station = []
+tickits = []
+
+station.append(Station("New Jalpaiguri", "NJP", "Siliguri"))
+station.append(Station("Howrah", "HWR", "Kolkata"))
+station.append(Station("Delhi", "DLH", "Delhi"))
+station.append(Station("Mumbai", "MBY", "Mumbai"))
+station.append(Station("Tata Nagar", "TATA", "Jamshedpur"))
+station.append(Station("Guwahati", "GHW", "Assam"))
+station.append(Station("Kashmir", "KSM", "Kashmir"))
+station.append(Station("Manali", "MNL", "Himachal"))
+
+print(station[0])
